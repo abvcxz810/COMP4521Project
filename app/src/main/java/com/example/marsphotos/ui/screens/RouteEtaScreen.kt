e387f767681cd7503d486e304caa3b118b3a941c
Giantabvcxz <abvcxz2019@gmail.com>
2022-12-01 20:07:35 +0800

    upload RouteEtaScreen.kt file

---
+++ app/src/main/java/com/example/marsphotos/ui/screens/RouteEtaScreen.kt
@@ -0,0 +1,77 @@
+package com.example.marsphotos.ui.screens
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.marsphotos.network.RouteData
+
+@Composable
+fun RouteEtaScreen(routeEtaUiState: RouteEtaUiState,modifier: Modifier = Modifier){
+    when (routeEtaUiState) {
+        is RouteEtaUiState.Success -> RouteEtaResultScreen(
+            routeEtaUiState.etaList,
+            modifier)
+        is RouteEtaUiState.Loading -> LoadingScreen(modifier)
+        is RouteEtaUiState.Error -> ErrorScreen(modifier)
+    }
+}
+
+/**
+ * The home screen displaying result of fetching photos.
+ */
+@Composable
+fun RouteEtaResultScreen(
+    etaList: List<EtaDataWithBusStopName>,
+    modifier: Modifier = Modifier,
+) {
+    EtaList(etaList)
+}
+
+@Composable
+fun EtaList(etas: List<EtaDataWithBusStopName>) {
+    LazyColumn {
+        items(etas) { eta ->
+            if (eta.eta.eta_seq == 1) ETAItem(eta = eta)
+        }
+    }
+}
+
+@Composable
+fun ETAItem(
+    eta: EtaDataWithBusStopName,
+    modifier: Modifier = Modifier,
+) {
+
+    Card(
+        modifier = modifier
+            .wrapContentHeight()
+            .fillMaxWidth()
+            .padding(8.dp),
+        elevation = 2.dp,
+        backgroundColor = Color.Gray
+    ) {
+        Row(modifier = Modifier.padding(16.dp)) {
+            Column(modifier = Modifier
+                .padding(end = 16.dp)
+                .weight(1f)) {
+                Text(text = eta.eta.route, style = MaterialTheme.typography.body1)
+                Text(text = "往${eta.eta.dest_tc}", style = MaterialTheme.typography.body1)
+                Text(text = eta.eta.seq.toString(), style = MaterialTheme.typography.body1)
+                Text(text = eta.busStopName, style = MaterialTheme.typography.body1)
+            }
+            Text(text = eta.eta.dir, style = MaterialTheme.typography.body1)
+            Spacer(modifier = Modifier.width(5.dp))
+            Text(text = eta.eta.eta_seq.toString(), style = MaterialTheme.typography.body1)
+            Spacer(modifier = Modifier.width(5.dp))
+            Text(text = extractTime(eta.eta.eta!!), style = MaterialTheme.typography.body1)
+            Spacer(modifier = Modifier.width(5.dp))
+        }
+    }
+}
