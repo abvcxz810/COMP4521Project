1abbf34c2901750fc2aca832a3bf25ba6394b06b
thsze <110726022+thsze@users.noreply.github.com>
2022-12-01 22:40:17 +0800

    Added a search bar on top of the route list
    search bar in HomeScreen.kt

--- app/src/main/java/com/example/marsphotos/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/marsphotos/ui/screens/HomeScreen.kt
@@ -21,16 +21,20 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import com.example.marsphotos.R
 import com.example.marsphotos.network.RouteData
@@ -42,7 +46,7 @@ fun HomeScreen(
     modifier: Modifier = Modifier,
 ) {
     when (marsUiState) {
-        is MarsUiState.Success -> ResultScreen1(
+        is MarsUiState.Success -> ResultScreen1( 
             onRouteItemClicked,
             marsUiState.etaList,
             marsUiState.routeDataList,
@@ -82,13 +86,77 @@ fun ResultScreen1(
     routeDataList: List<RouteData>,
     modifier: Modifier = Modifier,
 ) {
-    RouteDataList(routeDataList,onRouteItemClicked)
+    val textState = remember { mutableStateOf(TextFieldValue("")) }
+    Column {
+        SearchView(state = textState)
+        RouteDataList(routeDataList, onRouteItemClicked, textState)
+    }
+}
+
+// Search bar
+@Composable
+fun SearchView(
+    modifier: Modifier = Modifier,
+    state: MutableState<TextFieldValue>
+) {
+
+    TextField(
+        modifier = modifier.fillMaxWidth(),
+        value = state.value,
+        onValueChange = { value ->
+            state.value = value
+        },
+
+        // Search icon in front
+        leadingIcon = {
+            Icon(
+                Icons.Default.Search,
+                contentDescription = "",
+                modifier = Modifier
+                    .padding(15.dp)
+                    .size(24.dp)
+            )
+        },
+
+        // X icon for clearing search
+        trailingIcon = {
+            if (state.value != TextFieldValue("")) {
+                IconButton(
+                    // Clear search text on click
+                    onClick = {
+                        state.value = TextFieldValue("")
+                    }
+                ) {
+                    Icon(
+                        Icons.Default.Close,
+                        contentDescription = "",
+                        modifier = Modifier
+                            .padding(15.dp)
+                            .size(24.dp)
+                    )
+                }
+            }
+        }
+    )
 }
 
 @Composable
-fun RouteDataList(routeDataList: List<RouteData>, onRouteItemClicked: (String)->Unit) {
+fun RouteDataList(routeDataList: List<RouteData>, onRouteItemClicked: (String)->Unit,  state: MutableState<TextFieldValue>) {
+    var filteredRoutes: ArrayList<RouteData>
     LazyColumn {
-        items(routeDataList) { routeData ->
+        val searchedRoute = state.value.text
+        filteredRoutes =
+            if (searchedRoute.isEmpty()) {ArrayList(routeDataList)}
+            else{
+                val resultList = ArrayList<RouteData>()
+                for (route in routeDataList){
+                    if (route.route.contains(searchedRoute)){
+                        resultList.add(route)
+                    }
+            }
+                resultList
+            }
+        items(filteredRoutes) { routeData ->
             Surface(modifier = Modifier.clickable { onRouteItemClicked(routeData.route) }) {
                 RouteDataItem(routeData.route,routeData.orig_tc,routeData.orig_en,routeData.dest_tc,routeData.dest_en)
             }
