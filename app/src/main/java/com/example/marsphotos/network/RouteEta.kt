470cc1e9c539c6ddd45ac021c6c4e9e7a41e95ff
thsze <110726022+thsze@users.noreply.github.com>
2022-12-06 13:30:25 +0800

    missed file in previous commit
    oops

--- app/src/main/java/com/example/marsphotos/network/RouteEta.kt
+++ app/src/main/java/com/example/marsphotos/network/RouteEta.kt
@@ -90,3 +90,30 @@ data class RouteData(
     val dest_tc: String = "",
     val dest_sc: String = "",
 )
+
+@Serializable
+data class StopEtaList(
+    val type: String,
+    val version: String,
+    val generated_timestamp: String,
+    val data: List<StopEta>
+)
+
+@Serializable
+data class StopEta(
+    val co: String = "KMB",//The bus company
+    val route: String = "", //The bus route number of the requested bus company
+    val dir: String = "",//The direction of the bus route
+    val service_type: Int = 0,//The service type of the bus route.
+    val seq: Int = 0, //The stop sequence number of a bus route
+    val stop: String = "",
+    val dest_tc: String = "",//The destination of a bus route in Traditional Chinese
+    val dest_sc: String = "",//The destination of a bus route in Simplified Chinese.
+    val dest_en: String = "",//The destination of a bus route in English
+    val eta_seq: Int = 0,//The sequence number of ETA
+    val eta: String? = "-",//The timestamp of the next ETA
+    val rmk_tc: String = "",//The remark of an ETA in Traditional Chinese
+    val rmk_sc: String = "",//The remark of an ETA in Simplified Chinese.
+    val rmk_en: String = "",//The remark of an ETA in English
+    val data_timestamp: String = "",//The timestamp of the data when it was initially
+)
